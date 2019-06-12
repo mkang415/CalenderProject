829b7445eb9dd8068382fa1141afa79badfe2bed
Youngjin Mo <creator.youngjin@gmail.com>
2019-06-12 12:44:38 +0900

    fixed error

--- src/board/controller/MyBoardController.java
+++ src/board/controller/MyBoardController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 
 @WebServlet("/myboard")
-public class MainController extends HttpServlet {
+public class MyBoardController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	@Override

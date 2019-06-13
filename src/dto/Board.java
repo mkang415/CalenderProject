3b91c24357493f034c5905d51579da96839ecb06
mkang7 <kang7542@gmail.com>
2019-06-13 13:15:39 +0900

    board db변경에 맞춰서 dto 수정

--- src/dto/Board.java
+++ src/dto/Board.java
@@ -5,72 +5,70 @@ import java.util.Date;
 public class Board {
 
 	private int boardno; //게시판번호
-	private String userid; //유저아이디
+	private String nickname; //유저아이디
 	private String title; //글제목
 	private String content; //글본문
-	private int scheduleno; //스케줄넘버?
+	private Date gamedate;	//경기일자
+	private String team;	//직관 경기 팀
 	private Date insertdate; //글쓴날짜
 	private int hit; //조회수
 	
+	
 	@Override
 	public String toString() {
-		return "board [boardno=" + boardno + ", userid=" + userid + ", title=" + title + ", content=" + content
-				+ ", scheduleno=" + scheduleno + ", insertdate=" + insertdate + ", hit=" + hit + "]";
+		return "Board [boardno=" + boardno + ", nickname=" + nickname + ", title=" + title + ", content=" + content
+				+ ", gamedate=" + gamedate + ", team=" + team + ", insertdate=" + insertdate + ", hit=" + hit + "]";
 	}
 	
 	public int getBoardno() {
 		return boardno;
 	}
-	
 	public void setBoardno(int boardno) {
 		this.boardno = boardno;
 	}
-	
-	public String getUserid() {
-		return userid;
+	public String getNickname() {
+		return nickname;
 	}
-	
-	public void setUserid(String userid) {
-		this.userid = userid;
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
 	}
-	
 	public String getTitle() {
 		return title;
 	}
-	
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
 	public String getContent() {
 		return content;
 	}
-	
 	public void setContent(String content) {
 		this.content = content;
 	}
-	
-	public int getScheduleno() {
-		return scheduleno;
+	public Date getGamedate() {
+		return gamedate;
 	}
-	
-	public void setScheduleno(int scheduleno) {
-		this.scheduleno = scheduleno;
+	public void setGamedate(Date gamedate) {
+		this.gamedate = gamedate;
 	}
-	
+	public String getTeam() {
+		return team;
+	}
+	public void setTeam(String team) {
+		this.team = team;
+	}
 	public Date getInsertdate() {
 		return insertdate;
 	}
-	
 	public void setInsertdate(Date insertdate) {
 		this.insertdate = insertdate;
 	}
-	
 	public int getHit() {
 		return hit;
 	}
-	
 	public void setHit(int hit) {
 		this.hit = hit;
 	}
+	
+	
+	
 }
